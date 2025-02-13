6875093588927e21db501ac0f762b01dd37a04bc
luiginoIori <65583083+luiginoIori@users.noreply.github.com>
2025-02-13 18:33:18 -0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -38,15 +38,19 @@ for i in range(int(finan["Prazo"])):
    dados = {"Prazo" : prazo_fin, "Parcela" : parcelas }
 prazos = ["48","12","18","24","36","48","60"]
 
- 
-   
 with st.sidebar:  
       tela = 0
       
       st.title(":red[Comprar ou Alugar] :car:")      
       veiculo = st.selectbox("Veiculo ",tipos_carros)      
       valor = st.number_input("Valor", min_value=50000, max_value=250000, value=60000)
-      prazo = st.slider("Prazo Financiamento", 12, 60, 48) 
+      prazo = st.slider("Prazo Financiamento", 12, 60, 48)
+      km = st.number_input("Média de KM rodados por ANo",min_value=5000, max_value=80000, value=30000)
+      pneus = st.number_input("Valor troca de 4 pneus",min_value=500, max_value=8000, value=1800)
+      ano= ((pneus*((km*(prazo/12))/50000))/((km/12)*prazo))+0.1
+      print(ano)
+      manut = st.number_input("Valor de Manutenção por KM Rodado",min_value=0.010, max_value=2.00, value=ano)     
+       
       taxa = st.number_input("Taxa %", min_value=1.0, max_value=50.0, value=2.1)  
       valor_aluguel = st.number_input("Valor Aluguel", min_value=1000, max_value=6000, value=2500) 
       avaria = st.number_input("Avaria mensal veiculo alugado %", min_value=0.8, max_value=50.0, value=2.0) 
@@ -58,11 +62,10 @@ with st.sidebar:
       taxa=taxa/100
       reajuste=reajuste/100
       residual = residual/100
+      manutencao = (km * manut)/12
+      print(manutencao)
       for i in range(48):
          alugar.append(2600)
-         
-      
-      
       
       if st.button("Processar"):
          tela = 1
@@ -89,57 +92,58 @@ with st.sidebar:
                alugar.append(y)
                continue
             alugar.append(x)
-          
+         
          for i in range(int(prazo)): 
-            x=float(price_par[1])   
+            x=float(price_par[1]) 
+            x=x+manutencao  
             if i == 0:
-               x=x+premissa["1º Licenciamento"]
+               x=x+float(premissa["1º Licenciamento"])
             if i % 12 == 0 and i > 0: 
-               x=x+premissa["Licenciamento"] 
-               valor = valor + (valor*premissa["Reajuste"])
+               x=x+float(premissa["Licenciamento"] )
+               valor = valor + (valor*float(premissa["Reajuste"]))
                cont = 1
             if cont >=1 :
                if cont <=4 :
                   if price_par[0]=="PickUp":
-                     x=x+((valor*premissa["IPVA-Strada"])/4)                     
-                     x=x+((valor*premissa["Seguro"])/10)
+                     x=x+((valor*float(premissa["IPVA-Strada"]))/4)                     
+                     x=x+((valor*float(premissa["Seguro"]))/10)
                   if price_par[0]=="Popular":
-                     x=x+((valor*premissa["IPVA"])/4)
-                     x=x+((valor*premissa["Seguro"])/10)
+                     x=x+((valor*float(premissa["IPVA"]))/4)
+                     x=x+((valor*float(premissa["Seguro"]))/10)
                if cont>4<=10:     
-                  x=x+((valor*premissa["Seguro"])/10) 
+                  x=x+((valor*float(premissa["Seguro"]))/10) 
                cont = cont + 1
                if cont == 10 :
                   cont = 0              
             parcelas.append(x)
             prazo_fin.append(i+1)
             dados = {"Prazo" : prazo_fin, "Parcela" : parcelas }
-
+      
 df = pd.DataFrame(dict(dados))
 
 df["Alugar"] = alugar
 
 
-
+x = ["Parcela","Alugar"]
 if tela == 1:
    graf_linha_A = alt.Chart(df).mark_line(
-      point=alt.OverlayMarkDef(color="red",size=50, filled=False, fill="white"),
-      color="red"
+      point=alt.OverlayMarkDef(color="green",size=50, filled=False, fill="white"),
+      color="green"
       ).encode(
-      x= alt.X("Prazo", axis=alt.Axis(grid=False)),
-      y= alt.Y("Parcela",
-      axis=alt.Axis(grid=False),scale=alt.Scale(domain=(1000,3800))),
+      x= alt.X("Prazo", axis=alt.Axis(grid=False), title='Category'),
+      y= alt.Y("Parcela",               
+      axis=alt.Axis(grid=False),scale=alt.Scale(domain=(1000,3800))),               
       tooltip= ["Prazo","Parcela"],   
       
    ).properties(
       width=1000,
       height=400,
-      title= "Alugar x Comprar ",
+      
       
    )
    graf_linha_B = alt.Chart(df).mark_line(
-      point=alt.OverlayMarkDef(color="green",size=50, filled=False, fill="white"),
-      color="green"
+      point=alt.OverlayMarkDef(color="red",size=50, filled=False, fill="white"),
+      color="red"
       ).encode(
       x= alt.X("Prazo"), 
       y= alt.Y("Alugar"),
@@ -166,19 +170,18 @@ if tela == 1:
    col1.subheader("Alugar " )
    col2.subheader("Comprar" )
    result_alugar_comprar = (((soma_comprar-soma_aluguel)/soma_aluguel)*100)
-   if result_alugar_comprar >=0 :
-      col3.subheader("Alugar Melhor Comprar " )
-   else :
+   if soma_aluguel > soma_comprar :
       col3.subheader("Comprar Melhor Alugar " )
+   else :
+      col3.subheader("Alugar Melhor Comprar " )
    
    col1, col2, col3 = st.columns(3,vertical_alignment="top" , border=True)
    col1.subheader(":car: " + str(f'R$ {soma_aluguel:,.2f}') )
    col2.subheader(":car: " + str(f'R$ {soma_comprar:,.2f}') )
-   if result_alugar_comprar >=0 :
-      col3.subheader("Alugar " + str('% 0.2f' % (((soma_comprar-soma_aluguel)/soma_aluguel)*100)) + '%' + " Comprar ")
-   else :
+   if soma_aluguel > soma_comprar :
       col3.subheader("Comprar " + str('% 0.2f' % (((soma_comprar-soma_aluguel)/soma_aluguel)*100)) + '%' + " Alugar")
-      
+   else :
+      col3.subheader("Alugar " + str('% 0.2f' % (((soma_aluguel-soma_comprar)/soma_aluguel)*100)) + '%' + " Comprar")
    valor_residual = soma_comprar-(valor*residual)
    
    st.html('''<h3><p style="font-size: 32px;background-color:#ADD8E6;text-align:center;">
@@ -191,22 +194,26 @@ if tela == 1:
    col2.subheader("Comprar" )
    col3.subheader("Menos "+ str(int(residual*100))+ " % Residual" )
    result_alugar_comprar = (((soma_comprar-soma_aluguel)/soma_aluguel)*100)
-   if (((valor_residual-soma_aluguel)/soma_aluguel)*100) >=0 :
-      col4.subheader("Alugar Melhor Comprar" )
+   if soma_aluguel > (soma_comprar-valor_residual) :
+      col4.subheader("Melhor Comprar" )
    else :
-      col4.subheader("Comprar Melhor Alugar " )
+      col4.subheader("Melhor Alugar " )
    
    col1, col2, col3, col4 = st.columns([1,1,1.2,1.8], vertical_alignment="center", border=True )
    col1.subheader(":car: " + str(f'R$ {soma_aluguel:,.2f}') )
    col2.subheader(":car: " + str(f'R$ {soma_comprar:,.2f}') )
    col3.subheader(":car: " + str(f'R$ {valor_residual:,.2f}') )
-   if (((valor_residual-soma_aluguel)/soma_aluguel)*100) >=0 :
-      col4.subheader("Alugar " + str('% 0.2f' % (((valor_residual-soma_aluguel)/soma_aluguel)*100)) + '%' + " Comprar")
-   else :
+   if soma_aluguel > (soma_comprar-valor_residual) :
       col4.subheader("Comprar " + str('% 0.2f' % (((valor_residual-soma_aluguel)/soma_aluguel)*100)) + '%' + " Alugar")
+   else :
+      col4.subheader("Alugar " + str('% 0.2f' % (((valor_residual-soma_aluguel)/soma_aluguel)*100)) + '%' + " Comprar")
    pd.set_option("display.precision",2)
    def formatar(valor):
       return "$ {:,.2f}".format(valor)
   
+   
+   
+   
+
    
    
