aee26e6517e111f1a749fbee49c703ed8f85d020
luiginoIori <65583083+luiginoIori@users.noreply.github.com>
2025-05-19 21:20:08 -0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -132,7 +132,7 @@ if tela == 1:
       ).encode(
       x= alt.X("Prazo", axis=alt.Axis(grid=False), title='Category'),
       y= alt.Y("Parcela",               
-      axis=alt.Axis(grid=False),scale=alt.Scale(domain=(2200,4800))),               
+      axis=alt.Axis(grid=False),scale=alt.Scale(domain=(2500,4300))),               
       tooltip= ["Prazo","Parcela"],   
       
    ).properties(
